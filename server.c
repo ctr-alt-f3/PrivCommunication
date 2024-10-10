4cae344becca3181225ae3a88403860830d7abea
Therr <133943286+ctr-alt-f3@users.noreply.github.com>
2024-10-10 18:06:13 +0200

    added server.c file - still not meant to work

---
+++ server.c
@@ -0,0 +1,37 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 2137
+int main() {
+  int socketfp = socket(AF_INET, SOCK_STREAM, 0);
+  if (socketfp == -1) {
+    perror("socket initialisation failed\n");
+  };
+  // initialised socket
+  struct sockaddr_in socket_address_in;
+  socket_address_in.sin_addr.s_addr = INADDR_ANY;
+  socket_address_in.sin_port = htons(PORT);
+  socket_address_in.sin_family = AF_INET;
+  socklen_t len = sizeof(socket_address_in);
+  if (bind(socketfp, (struct sockaddr *)&socket_address_in, len) == -1) {
+    printf("binding to port %d failed\n", PORT);
+    // binded socket
+  }
+}
+/* TODO :
+  -getting data (using all supported ports)
+  -decrypting data
+
+
+
+
+
+
+
+*/
