29b69d928c6fad72e74ba0c9b9d56a45a7506cf6
Maciej <swmikolaj909@gmail.com>
2024-10-10 19:29:35 +0200

    added .gitignore and client

--- client.c
+++ client.c
@@ -1,4 +1,5 @@
 #include <arpa/inet.h>
+#include <netdb.h>
 #include <netinet/in.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -13,15 +14,27 @@ int main() {
   if (socketfp == -1) {
     perror("socket initialisation failed\n");
   };
+  // socket initialisation
+  struct hostent *server = gethostbyname("localhost");
+  struct sockaddr_in addr_struct;
+  addr_struct.sin_family = AF_INET;
+  addr_struct.sin_port =
+      PORT; ////////////////////////////////////////////////////////////////////////////////////////
+  bcopy((char *)server->h_addr, (char *)&addr_struct.sin_addr.s_addr,
+        server->h_length);
+  // sockaddr_in initialisation
+  if (!!connect(socketfp, &addr_struct, sizeof(socketfp))) {
+    perror("connection failed\n");
+  }
 
-/*
-  TODO:
-  -basic i/o via network without encryption
-  -adding encryption
-  -spoofing as http requests
-  -adding other methods
-  -making method used random
-  -finishing
-  -some other weird stuff
-
-*/
+  /*
+    TODO:
+    -basic i/o via network without encryption
+    -adding encryption
+    -spoofing as http requests
+    -adding other methods
+    -making method used random
+    -finishing
+    -add peers
+  */
+}
