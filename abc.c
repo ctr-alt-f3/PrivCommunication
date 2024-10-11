69fa84181ab226ee4f12579462fd5c4527c9c5d4
Therr <swmikolaj909@gmail.com>
2024-10-11 16:01:30 +0200

    Delete abc.c

--- abc.c
+++
@@ -1,5 +0,0 @@
-int asdaso[];
-pjasdjpas ijdoasjda;
-agfd zvx;
-tcan try to map tab in normal modcan try to map tab in normal modcan try to map
-    tab in normal modcan try to map tab in normal mod
